b6ff0fb9ac69542a067c3fb22f06e4d5c8d14c75
Eskimo <Eskimo0C0ld@gmail.com>
2022-11-11 15:10:52 +0300

    fixed config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # CONFIG FILE
 
-TOKEN  = "5514267020:AAGRCdzgUt_2bPbwNZtsCeImRlhnbZ4LpRM" # Telegrambot API Token
+TOKEN  = "<YOUR TOKEN>" # Telegrambot API Token
 MINKWLEN = 0 # How many keywords will be used(0 - max)
 NONE_STOP = False # Bot would be restarted if it crashed
